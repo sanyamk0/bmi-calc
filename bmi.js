92355b583f21cc100f743a455fc7778afb84ef79
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-03-31 02:22:26 +0530

    formatting done

--- bmi.js
+++ bmi.js
@@ -1,28 +1,25 @@
 function calculate() {
-    var weight = document.getElementById('weight').value;
-    document.getElementById('span-weight-val').innerText = weight + " Kg";
+  var weight = document.getElementById("weight").value;
+  document.getElementById("span-weight-val").innerText = weight + " Kg";
 
-    var height = document.getElementById('height').value;
-    document.getElementById('span-height-val').innerText = height + " cm";
+  var height = document.getElementById("height").value;
+  document.getElementById("span-height-val").innerText = height + " cm";
 
-    var bmi = (weight / Math.pow((height / 100), 2)).toFixed(1);
-    bmivalue.innerText = bmi;
+  var bmi = (weight / Math.pow(height / 100, 2)).toFixed(1);
+  bmivalue.innerText = bmi;
 
-    if (bmi < 18.5) {
-        category = "Underweight";
-        bmivalue.style.color = "#ffc44d";
-    }
-    else if (bmi >= 18.5 && bmi <= 24.9) {
-        category = "Normal Weight";
-        bmivalue.style.color = "#0be881";
-    }
-    else if (bmi >= 25 && bmi <= 29.9) {
-        category = "Overweight";
-        bmivalue.style.color = "#ff884d";
-    }
-    else {
-        category = "Obese";
-        bmivalue.style.color = "#ff5e57";
-    }
-    document.getElementById('category').innerText = category;
+  if (bmi < 18.5) {
+    category = "Underweight";
+    bmivalue.style.color = "#ffc44d";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = "Normal Weight";
+    bmivalue.style.color = "#0be881";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    category = "Overweight";
+    bmivalue.style.color = "#ff884d";
+  } else {
+    category = "Obese";
+    bmivalue.style.color = "#ff5e57";
+  }
+  document.getElementById("category").innerText = category;
 }
